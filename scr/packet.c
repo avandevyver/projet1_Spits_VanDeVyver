0c2ef5dc12c03c17e5a01149beac80ea66298dd1
arnaud spits <arnaudspits@gmail.com>
2018-10-18 17:27:23 +0200

    packet.c_done_(_to_Verify_)

--- scr/packet.c
+++ scr/packet.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <zlib.h>
 
 /* Extra #includes */
 /* Your code will be inserted here */
@@ -24,22 +25,22 @@ pkt_status_code pkt_validity(const pkt_t *pkt) {
 		return E_UNCONSISTENT;
 	}
 	if ((pkt->type < 1) || (pkt->type > 3)) {
-		return E_TYPE;
+		return E_TYPE;   /** Type   match range **/
 	}
 	if ((pkt->tr != 1) && (pkt->tr != 0)) {
-		return E_TR;
+		return E_TR;     /** Tr     match range **/
 	}
 	if ((pkt->type == PTYPE_DATA) && (pkt->tr != 0)) {
-		return E_TR;
+		return E_TR;     /** Tr     match       **/
 	}
 	if ((pkt->window < 0) || (pkt->window > 31)) {
-		return E_WINDOW;
+		return E_WINDOW; /** Window match range **/
 	}
 	if ((pkt->seqnum < 0) || (pkt->seqnum > 255)) {
-		return E_SEQNUM;
+		return E_SEQNUM; /** Seqnum match range **/
 	}
 	if ((pkt->length < 0) || (pkt->length > 512)) {
-		return E_LENGTH;
+		return E_LENGTH; /** Length match range **/
 	}
 	return 0;
 }
@@ -74,9 +75,11 @@ void pkt_del(pkt_t *pkt)
 
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
+	/** Check size data **/
 	if (len < 12 * sizeof(char)) {
 		return E_NOHEADER;
 	}
+	/** Prevente NULL Pointer exeption**/
 	if (pkt == NULL) {
 		pkt = pkt_new();
 		if (pkt == NULL) {
@@ -86,35 +89,57 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 	else if (pkt->playload != NULL) {
 		free(pkt->playload);
 	}
-	memcpy(pkt, data, 12 * sizeof(char));/*copy header*/
+	/**== Copy Header ==**/
+	memcpy(pkt, data, 12 * sizeof(char));
 	pkt->length = ntohs(pkt->length);
-	if ((len != 12 * sizeof(char)) || (len < (16 + pkt->length) * sizeof(char))) {
-		return E_UNCONSISTENT;
-	}
-	pkt->playload = malloc(pkt->length * sizeof(char));
-	if (pkt->playload == NULL) {
-		return E_NOMEM;
-	}
 	pkt->crc1 = ntohl(pkt->crc1);
-
-	char * tmp = &(data[12]);
-	memcpy(pkt->playload, tmp, pkt->length * sizeof(char));
-	tmp = &(tmp[pkt->length]);
-	memcpy(&(pkt->crc2), tmp, 4 * sizeof(char));
-	pkt->crc2 = ntohl(pkt->crc2);
-
-	/*calc crc1 crc2 + check*/
-
-	return pkt_validity(pkt);
-}
-
-pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
-{
+	/** Check validity header **/
 	pkt_status_code dia = pkt_validity(pkt);
 	if (dia != 0) {
 		return dia;
 	}
+	/** Check size data **/
+	if ((len != 12 * sizeof(char)) || (len < (16 + pkt->length) * sizeof(char))) {
+		return E_UNCONSISTENT;
+	}
+	/** Check CRC 1 **/
+	if (pkt->tr == 0) {
+		if (pkt->crc1 != crc32(0, data, 8 * sizeof(char))) {
+			return E_CRC;
+		}
+	}
+	/**== Carry playload ==**/
+	if (len != 12 * sizeof(char)) {
 
+		/** Copy Playload **/
+		pkt->playload = malloc(pkt->length * sizeof(char));
+		if (pkt->playload == NULL) {
+			return E_NOMEM;
+		}
+		char * tmp = &(data[12]);
+		memcpy(pkt->playload, tmp, pkt->length * sizeof(char));
+		tmp = &(tmp[pkt->length]);
+		memcpy(&(pkt->crc2), tmp, 4 * sizeof(char));
+		pkt->crc2 = ntohl(pkt->crc2);
+		/** Check CRC 2 **/
+		if (pkt->tr == 0) {
+			if (pkt->crc2 != crc32(0, pkt->playload, pkt->length * sizeof(char))) {
+				return E_CRC;
+			}
+		}
+	}
+
+	return 0;
+}
+
+pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
+{
+	/** Check validity header **/
+	pkt_status_code dia = pkt_validity(pkt);
+	if (dia != 0) {
+		return dia;
+	}
+	/** Size evaluation **/
 	int pkt_size = 12 + pkt->length;
 	if (pkt->length != 0) {
 		pkt_size += 4;
@@ -122,22 +147,28 @@ pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 	if (len < pkt_size * sizeof(char)) {
 		return E_NOMEM;
 	}
-
-	memcpy(buf, pkt, 2);/*copy type-tr-window*/
+	/**== Paste Header ==**/
+	memcpy(buf, pkt, 2);                                  /* type-tr-window */
 	char * tmp = &(buf[2]);
-	memcpy(tmp, &(htons(pkt->length)), 2 * sizeof(char));
+	memcpy(tmp, &(htons(pkt->length)), 2 * sizeof(char)); /*     length     */
 	tmp = &(tmp[2]);
-	memcpy(tmp, &(pkt->timestamp), 4 * sizeof(char));
+	memcpy(tmp, &(pkt->timestamp), 4 * sizeof(char));     /*    timestamp   */
 	tmp = &(tmp[4]);
-	/*calc crc1*/
-	memcpy(tmp, &(htonl(pkt->crc1)), 4 * sizeof(char));
+	/** Calcule crc 1 **/
+	pkt->crc1 = crc32(0, buf, 8 * sizeof(char));
+	memcpy(tmp, &(htonl(pkt->crc1)), 4 * sizeof(char));   /*     crc 1      */
 	tmp = &(tmp[4]);
+
+	/**== Carry playload ==**/
 	if (l > 0) {
-		/*calc crc2*/
-		memcpy(tmp, pkt->playload, pkt->length * sizeof(char));
+		/** Calcule crc 2 **/
+		pkt->crc2 = crc32(0, pkt->playload, pkt->length * sizeof(char));
+		memcpy(tmp, pkt->playload, pkt->length * sizeof(char)); /* playload */
 		tmp = &(tmp[pkt->length]);
-		memcpy(tmp, &(htons(pkt->crc2)), 4 * sizeof(char));
+		memcpy(tmp, &(htons(pkt->crc2)), 4 * sizeof(char));     /*  crc 2   */
 	}
+	/** Bytes copy in buf**/
+	len = pkt_size;
 	return 0;
 }
 
